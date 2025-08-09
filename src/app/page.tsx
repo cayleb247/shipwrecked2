afd2c39fb21b4a6bb69ce56da54693c0bcf65281
Nathan Cloutier <nathanc4571@gmail.com>
2025-08-09 15:16:02 -0400

    Move variables out of emoji picker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,59 @@
 "use client"
 
 import styles from "./page.module.css";
-import * as React from 'react';
+import { useState } from "react";
 import EmojiPicker from '@/components/EmojiPicker';
 
-export default function Home() {  
-
+export default function Home() {
   const object_emojis = ["🚃", "👨", "🐕", "✈️", "🌮", "📕"];
   const action_emojis = ["🏃‍➡️", "🏊", "🚶‍♂️", "🤣"];
 
+  const [emoji1, setEmoji1] = useState<string>(object_emojis[0]);
+  const [emoji1Emotion, setEmoji1Emotion] = useState(50);
+  const [emoji1Arousal, setEmoji1Arousal] = useState(50);
+  const [emoji2, setEmoji2] = useState<string>(action_emojis[0]);
+  const [emoji2Emotion, setEmoji2Emotion] = useState(50);
+  const [emoji2Arousal, setEmoji2Arousal] = useState(50);
+  const [emoji3, setEmoji3] = useState<string>(object_emojis[1]);
+  const [emoji3Emotion, setEmoji3Emotion] = useState(50);
+  const [emoji3Arousal, setEmoji3Arousal] = useState(50);
 
   return (
     <div className={styles.page}>
       <div className="h-32 flex flex-row gap-4 text-lg">
-        <div onClick={() => {}}>
+        <div onClick={() => { }}>
           <div>
-            <EmojiPicker emojis={object_emojis} />
-          </div>
-        </div>
-
-        <div className="flex items-center justify-center h-full">
-          <p className="text-3xl">+</p>
-        </div> 
-
-        <div>
-          <div>
-            <EmojiPicker emojis={action_emojis} />
+            <EmojiPicker selectedEmoji={emoji1} emojiList={object_emojis} setEmoji={setEmoji1} />
           </div>
         </div>
 
         <div className="flex items-center justify-center h-full">
           <p className="text-3xl">+</p>
         </div>
-      
+
         <div>
           <div>
-            🟩
+            <EmojiPicker selectedEmoji={emoji2} emojiList={action_emojis} setEmoji={setEmoji2} />
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center h-full">
+          <p className="text-3xl">+</p>
+        </div>
+
+        <div>
+          <div>
+            <EmojiPicker selectedEmoji={emoji3} emojiList={action_emojis} setEmoji={setEmoji3} />
           </div>
         </div>
 
         <div className="flex items-center">
-            <button className="bg-green-200 rounded-full text-white w-16 h-16 hover:bg-green-400 transition-colors duration-300">
+          <button className="bg-green-200 rounded-full text-white w-16 h-16 hover:bg-green-400 transition-colors duration-300">
             <span className="text-black"> = </span>
-            </button>
+          </button>
         </div>
       </div>
-      
+
     </div>
   );
 }
--- src/components/EmojiPicker.tsx
+++ src/components/EmojiPicker.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Button } from "@/components/ui/button"
 import {
     Dialog,
@@ -6,23 +6,26 @@ import {
     DialogTrigger,
 } from "@/components/ui/dialog"
 
-export default function EmojiPicker({ emojis }: { emojis: string[] }) {
-    const [currentEmoji, setCurrentEmoji] = useState(emojis[0]);
+export default function EmojiPicker({ selectedEmoji, emojiList, setEmoji }: { selectedEmoji: string, emojiList: string[], setEmoji: (emoji: string) => void }) {
     const [isDialogOpen, setIsDialogOpen] = useState(false);
 
+    useEffect(() => {
+        console.log(selectedEmoji)
+    }, [selectedEmoji])
+
     const pickEmoji = (emoji: string) => {
-        if (!emojis.includes(emoji)) return
-        setCurrentEmoji(emoji);
+        if (!emojiList.includes(emoji)) return
+        setEmoji(emoji)
         setIsDialogOpen(false);
     }
 
     return (
         <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
             <DialogTrigger className={"w-32 h-32"}>
-                <Button className={"w-32 h-32 text-8xl bg-black/5 hover:bg-black/20"}>{currentEmoji}</Button>
+                <Button className={"w-32 h-32 text-8xl bg-black/5 hover:bg-black/20"}>{selectedEmoji}</Button>
             </DialogTrigger>
             <DialogContent className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-6 p-4 h-[calc(75%)] w-full">
-                {emojis.map(emoji => (
+                {emojiList.map(emoji => (
                     <Button key={emoji} onClick={() => pickEmoji(emoji)} className={"w-16 h-16 text-4xl bg-black/0 hover:bg-black/20"}>
                         {emoji}
                     </Button>
