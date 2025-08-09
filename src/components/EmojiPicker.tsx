5a63d97be98992be84b4a5c9fdbcce95145341fb
Nathan Cloutier <nathanc4571@gmail.com>
2025-08-09 12:55:25 -0400

    Unfinished dialog for picking emoji

--- src/components/EmojiPicker.tsx
+++ src/components/EmojiPicker.tsx
@@ -1,32 +1,33 @@
 import { useState } from 'react';
 import { Button } from "@/components/ui/button"
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+    Dialog,
+    DialogContent,
+    DialogTrigger,
+} from "@/components/ui/dialog"
 
 export default function EmojiPicker({ emojis }: { emojis: string[] }) {
     const [currentEmoji, setCurrentEmoji] = useState(emojis[0]);
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
 
     const pickEmoji = (emoji: string) => {
         if (!emojis.includes(emoji)) return
         setCurrentEmoji(emoji);
-        setIsDropdownOpen(false);
+        setIsDialogOpen(false);
     }
 
     return (
-        <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
-            <DropdownMenuTrigger asChild>
-                <Button>{currentEmoji}</Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
+        <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+            <DialogTrigger className={"w-32 h-32"}>
+                <Button className={"w-32 h-32 text-8xl bg-black/0 hover:bg-black/20"}>{currentEmoji}</Button>
+            </DialogTrigger>
+            <DialogContent className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 p-4 h-calc[75%])">
                 {emojis.map(emoji => (
-                    <DropdownMenuItem key={emoji} onClick={() => pickEmoji(emoji)}>{emoji}</DropdownMenuItem>
+                    <Button key={emoji} onClick={() => pickEmoji(emoji)} className={"text-6xl bg-black/0 hover:bg-black/20"}>
+                        {emoji}
+                    </Button>
                 ))}
-            </DropdownMenuContent>
-        </DropdownMenu>
+            </DialogContent>
+        </Dialog>
     )
 }
