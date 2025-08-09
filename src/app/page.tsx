fc0eeb8d1d596926ec2b6d963921e7eec3686a06
Caleb Wang <156046197+cayleb247@users.noreply.github.com>
2025-08-09 19:31:52 -0400

    Changed UI to black

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,22 @@
-"use client"
+"use client";
 
 import styles from "./page.module.css";
 import { useState } from "react";
-import EmojiPicker from '@/components/EmojiPicker';
+import EmojiPicker from "@/components/EmojiPicker";
 import React from "react";
 import EmotionSlider from "@/components/EmotionSlider";
 import ArouselSlider from "@/components/ArouselSlider";
 
 export default function Home() {
-
   const [loading, setLoading] = useState(false);
   const object_emojis = ["🚃", "👨", "🐕", "✈️", "🌮", "📕"];
   const action_emojis = ["🏃‍➡️", "🏊", "🚶‍♂️", "🤣"];
   const [imageUrl, setImageUrl] = useState<string | null>(null);
 
   const handleGenerate = async () => {
-    const response = await fetch('/api/image', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+    const response = await fetch("/api/image", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         subject: emoji1,
         subjectEmotion: emoji1Emotion,
@@ -27,15 +26,15 @@ export default function Home() {
         verbArousal: emoji2Arousal,
         object: emoji3,
         objectEmotion: emoji3Emotion,
-        objectArousal: emoji3Arousal
-      })
+        objectArousal: emoji3Arousal,
+      }),
     });
 
     console.log(response);
     const url = await response.json();
     console.log(url);
     setImageUrl(url);
-  }
+  };
 
   const [emoji1, setEmoji1] = useState<string>(object_emojis[0]);
   const [emoji1Emotion, setEmoji1Emotion] = useState([50]);
@@ -49,66 +48,115 @@ export default function Home() {
 
   return (
     <div className={styles.page}>
-      <div className="h-32 flex flex-row gap-4 text-lg w-screen">
-        <div className="flex flex-row w-1/4 gap-2">
-          <EmojiPicker selectedEmoji={emoji1} emojiList={object_emojis} setEmoji={setEmoji1} setEmojiEmotion={setEmoji1Emotion} setEmojiArousal={setEmoji1Arousal} />
-          <div className="flex flex-col w-2/3">
-            <EmotionSlider value={emoji1Emotion} defaultValue={[50]} onValueChange={setEmoji1Emotion} />
-            <ArouselSlider value={emoji1Arousal} defaultValue={[50]} onValueChange={setEmoji1Arousal} />
+      <div className="h-full flex flex-col content-center justify-center col-start-1 col-end-1 row-start-1 row-end-2 gap-3">
+        <div className="flex gap-3 w-full justify-center">
+          <h1 className="text-[3rem]">😀</h1>
+          <h1 className="text-[3rem]">➡️</h1>
+          <h1 className="text-[3rem]">🖼️</h1>
+        </div>
+        
+        <div className="h-32 flex flex-row gap-4 text-lg w-screen justify-center">
+          <div className="flex flex-row w-1/4 gap-2">
+            <EmojiPicker
+              selectedEmoji={emoji1}
+              emojiList={object_emojis}
+              setEmoji={setEmoji1}
+              setEmojiEmotion={setEmoji1Emotion}
+              setEmojiArousal={setEmoji1Arousal}
+            />
+            <div className="flex flex-col w-2/3 justify-center">
+              <EmotionSlider
+                value={emoji1Emotion}
+                defaultValue={[50]}
+                onValueChange={setEmoji1Emotion}
+              />
+              <ArouselSlider
+                value={emoji1Arousal}
+                defaultValue={[50]}
+                onValueChange={setEmoji1Arousal}
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-center h-full">
+            <p className="text-3xl">+</p>
+          </div>
+
+          <div className="flex flex-row w-1/4 gap-2">
+            <EmojiPicker
+              selectedEmoji={emoji2}
+              emojiList={action_emojis}
+              setEmoji={setEmoji2}
+              setEmojiEmotion={setEmoji2Emotion}
+              setEmojiArousal={setEmoji2Arousal}
+            />
+            <div className="flex flex-col w-2/3 justify-center">
+              <EmotionSlider
+                value={emoji2Emotion}
+                defaultValue={[50]}
+                onValueChange={setEmoji2Emotion}
+              />
+              <ArouselSlider
+                value={emoji2Arousal}
+                defaultValue={[50]}
+                onValueChange={setEmoji2Arousal}
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-center h-full">
+            <p className="text-3xl">+</p>
+          </div>
+
+          <div className="flex flex-row w-1/4 gap-2">
+            <EmojiPicker
+              selectedEmoji={emoji3}
+              emojiList={action_emojis}
+              setEmoji={setEmoji3}
+              setEmojiEmotion={setEmoji3Emotion}
+              setEmojiArousal={setEmoji3Arousal}
+            />
+            <div className="flex flex-col w-2/3 justify-center">
+              <EmotionSlider
+                value={emoji3Emotion}
+                defaultValue={[50]}
+                onValueChange={setEmoji3Emotion}
+              />
+              <ArouselSlider
+                value={emoji3Arousal}
+                defaultValue={[50]}
+                onValueChange={setEmoji3Arousal}
+              />
+            </div>
           </div>
         </div>
-
-        <div className="flex items-center justify-center h-full">
-          <p className="text-3xl">+</p>
+        <div
+          className="flex justify-center content-center"
+          onClick={() => {
+            setLoading(true);
+            handleGenerate();
+          }}
+        >
+          <button className="bg-green-200 rounded-full text-white w-16 h-16 hover:bg-green-400 transition-all duration-300 hover:w-20 hover:h-20">
+            <span className="text-black"> = </span>
+          </button>
         </div>
-
-        <div className="flex flex-row w-1/4 gap-2">
-          <EmojiPicker selectedEmoji={emoji2} emojiList={action_emojis} setEmoji={setEmoji2} setEmojiEmotion={setEmoji2Emotion} setEmojiArousal={setEmoji2Arousal} />
-          <div className="flex flex-col w-2/3">
-            <EmotionSlider value={emoji2Emotion} defaultValue={[50]} onValueChange={setEmoji2Emotion} />
-            <ArouselSlider value={emoji2Arousal} defaultValue={[50]} onValueChange={setEmoji2Arousal} />
-          </div>
-        </div>
-
-        <div className="flex items-center justify-center h-full">
-          <p className="text-3xl">+</p>
-        </div>
-
-        <div className="flex flex-row w-1/4 gap-2">
-          <EmojiPicker selectedEmoji={emoji3} emojiList={action_emojis} setEmoji={setEmoji3} setEmojiEmotion={setEmoji3Emotion} setEmojiArousal={setEmoji3Arousal} />
-          <div className="flex flex-col w-2/3">
-            <EmotionSlider value={emoji3Emotion} defaultValue={[50]} onValueChange={setEmoji3Emotion} />
-            <ArouselSlider value={emoji3Arousal} defaultValue={[50]} onValueChange={setEmoji3Arousal} />
-          </div>
-        </div>
-
-        <div className="flex items-center" onClick={() => { setLoading(true); handleGenerate(); }}>
-            <button className="bg-green-200 rounded-full text-white w-16 h-16 hover:bg-green-400 transition-all duration-300 hover:w-20 hover:h-20">
-              <span className="text-black"> = </span>
-            </button>
-          </div>
       </div>
 
       <div className="flex flex-col items-center">
-        <div> 
-          {
-            loading &&
+        <div>
+          {loading && (
             <div className="spinner">
               <div className="spinnerin"></div>
             </div>
-          }
-          {imageUrl && 
+          )}
+          {imageUrl && (
             <div className="w-64 h-64 rounded-lg">
-              <img src={imageUrl} onLoad={() => setLoading(false)}/>
+              <img src={imageUrl} onLoad={() => setLoading(false)} />
             </div>
-          }
-        </div>
-
-        <div>
-          {/* Webcam */}
+          )}
         </div>
       </div>
-
     </div>
   );
 }
