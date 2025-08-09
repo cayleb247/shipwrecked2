859606431a25c40a335d6d0fdc723f81ff7ccd1c
Vedansh Mannem <mannem.vedansh@gmail.com>
2025-08-09 17:45:51 -0400

    ai prompt

--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -3,21 +3,30 @@ const openai = new OpenAI({ apiKey: process.env.CHATGPT_API_KEY });
 
 interface bodyType {
     subject: string,
+    subjectEmotion: string,
+    subjectArousal: string,
     verb: string,
+    verbEmotion: string,
+    verbArousal: string,
     object?: string,
-    emotion?: string
+    objectEmotion?: string,
+    objectArousal?: string,
 }
 
 export async function POST(request: Request) {
   // Parse the request body
   const body:bodyType = await request.json();
-  const { subject, verb, object, emotion } = body;
+  const { subject, verb, object, subjectEmotion, subjectArousal, verbEmotion, verbArousal, objectEmotion, objectArousal } = body;
 
-  console.log("body:", subject, verb, object, emotion);
+  console.log("body:", subject, verb, object, subjectEmotion, subjectArousal, verbEmotion, verbArousal, objectEmotion, objectArousal);
 
   const result = await openai.images.generate({
     model: "dall-e-3",
-    prompt: `Using the following subject, verb, and/or object and emotion, create an image that depicts what is going on. The subject, verb, and/or object and emotion are: ${subject}, ${verb}, ${object}, ${emotion}`,
+    prompt: `You are generating an image to communicate the sentiment expressed by a sentence written with emojis 
+    instead of text. The emojis are subject: ${subject}, verb: ${verb}, object: ${object}. Here is the emotion that is 
+    trying to be conveyed with each of those emojis: subjectEmotion: ${subjectEmotion}, subjectArousal: ${subjectArousal}, 
+    verbEmotion: ${verbEmotion}, verbArousal: ${verbArousal}, objectEmotion: ${objectEmotion}, objectArousal: ${objectArousal}.
+    Emotion is on a scale of 0-100 from unhappy to happy (50 is neutral). Arousal is 0-100 (deactivated to activated).`,
     size: "1024x1024",
   });
   console.log(result.data![0].url);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,38 @@
 import styles from "./page.module.css";
 import { useState } from "react";
 import EmojiPicker from '@/components/EmojiPicker';
+import React from "react";
 
 export default function Home() {
 
   const [loading, setLoading] = React.useState(false);
   const object_emojis = ["🚃", "👨", "🐕", "✈️", "🌮", "📕"];
   const action_emojis = ["🏃‍➡️", "🏊", "🚶‍♂️", "🤣"];
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
 
-  // React.useEffect(async () => {
-  //   const response = await fetch('/api/image', {
-  //     method: 'POST',
-  //     headers: { 'Content-Type': 'application/json' },
-  //     body: JSON.stringify({})
-  //   })
-  // }, [loading==true]);
+  const handleGenerate = async () => {
+    const response = await fetch('/api/image', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        subject: emoji1,
+        subjectEmotion: emoji1Emotion,
+        subjectArousal: emoji1Arousal,
+        verb: emoji2,
+        verbEmotion: emoji2Emotion,
+        verbArousal: emoji2Arousal,
+        object: emoji3,
+        objectEmotion: emoji3Emotion,
+        objectArousal: emoji3Arousal
+      })
+    });
+
+    console.log(response);
+    const url = await response.json();
+    console.log(url);
+    setImageUrl(url);
+  }
+
   const [emoji1, setEmoji1] = useState<string>(object_emojis[0]);
   const [emoji1Emotion, setEmoji1Emotion] = useState(50);
   const [emoji1Arousal, setEmoji1Arousal] = useState(50);
@@ -29,7 +47,7 @@ export default function Home() {
 
   return (
     <div className={styles.page}>
-      <div className="h-32 flex flex-row gap-4 text-lg">
+      <div className="h-32 flex flex-row gap-10 text-lg">
         <div onClick={() => { }}>
           <div>
             <EmojiPicker selectedEmoji={emoji1} emojiList={object_emojis} setEmoji={setEmoji1} />
@@ -50,31 +68,32 @@ export default function Home() {
           <p className="text-3xl">+</p>
         </div>
 
-        <div>
+        <div className="mr">
           <div>
             <EmojiPicker selectedEmoji={emoji3} emojiList={action_emojis} setEmoji={setEmoji3} />
           </div>
         </div>
 
-        {loading ? (
-          <div className="flex items-center">
-            <div className="spinner">
-              <div className="spinnerin"></div>
-            </div>
-          </div>
-        ) : (
-          <div className="flex items-center" onClick={() => setLoading(true)}>
-            <button className="bg-green-200 rounded-full text-white w-16 h-16 hover:bg-green-400 transition-colors duration-300">
-            <span className="text-black"> = </span>
+        <div className="flex items-center" onClick={() => { setLoading(true); handleGenerate(); }}>
+            <button className="bg-green-200 rounded-full text-white w-16 h-16 hover:bg-green-400 transition-all duration-300 hover:w-20 hover:h-20">
+              <span className="text-black"> = </span>
             </button>
           </div>
-        )}
-
       </div>
 
       <div className="flex flex-col items-center">
         <div> 
-          {/* Open AI image */}
+          {
+            loading &&
+            <div className="spinner">
+              <div className="spinnerin"></div>
+            </div>
+          }
+          {loading && imageUrl && 
+            <div className="w-64 h-64 rounded-lg">
+              <img src={imageUrl} onLoad={() => setLoading(false)}/>
+            </div>
+          }
         </div>
 
         <div>
