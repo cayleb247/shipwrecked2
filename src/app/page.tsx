dfaaa7884d3b77600d15da1ff87f0068a5d74306
Nathan Cloutier <nathanc4571@gmail.com>
2025-08-09 17:40:54 -0400

    Add emotion and arousal sliders to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@
 import styles from "./page.module.css";
 import { useState } from "react";
 import EmojiPicker from '@/components/EmojiPicker';
+import EmotionSlider from "@/components/EmotionSlider";
+import ArouselSlider from "@/components/ArouselSlider";
 
 export default function Home() {
 
-  const [loading, setLoading] = React.useState(false);
+  const [loading, setLoading] = useState(false);
   const object_emojis = ["🚃", "👨", "🐕", "✈️", "🌮", "📕"];
   const action_emojis = ["🏃‍➡️", "🏊", "🚶‍♂️", "🤣"];
 
@@ -18,21 +20,23 @@ export default function Home() {
   //   })
   // }, [loading==true]);
   const [emoji1, setEmoji1] = useState<string>(object_emojis[0]);
-  const [emoji1Emotion, setEmoji1Emotion] = useState(50);
-  const [emoji1Arousal, setEmoji1Arousal] = useState(50);
+  const [emoji1Emotion, setEmoji1Emotion] = useState([50]);
+  const [emoji1Arousal, setEmoji1Arousal] = useState([50]);
   const [emoji2, setEmoji2] = useState<string>(action_emojis[0]);
-  const [emoji2Emotion, setEmoji2Emotion] = useState(50);
-  const [emoji2Arousal, setEmoji2Arousal] = useState(50);
+  const [emoji2Emotion, setEmoji2Emotion] = useState([50]);
+  const [emoji2Arousal, setEmoji2Arousal] = useState([50]);
   const [emoji3, setEmoji3] = useState<string>(object_emojis[1]);
-  const [emoji3Emotion, setEmoji3Emotion] = useState(50);
-  const [emoji3Arousal, setEmoji3Arousal] = useState(50);
+  const [emoji3Emotion, setEmoji3Emotion] = useState([50]);
+  const [emoji3Arousal, setEmoji3Arousal] = useState([50]);
 
   return (
     <div className={styles.page}>
-      <div className="h-32 flex flex-row gap-4 text-lg">
-        <div onClick={() => { }}>
-          <div>
-            <EmojiPicker selectedEmoji={emoji1} emojiList={object_emojis} setEmoji={setEmoji1} />
+      <div className="h-32 flex flex-row gap-4 text-lg w-screen">
+        <div className="flex flex-row w-1/4 gap-2">
+          <EmojiPicker selectedEmoji={emoji1} emojiList={object_emojis} setEmoji={setEmoji1} setEmojiEmotion={setEmoji1Emotion} setEmojiArousal={setEmoji1Arousal} />
+          <div className="flex flex-col w-2/3">
+            <EmotionSlider value={emoji1Emotion} defaultValue={[50]} onValueChange={setEmoji1Emotion} />
+            <ArouselSlider value={emoji1Arousal} defaultValue={[50]} onValueChange={setEmoji1Arousal} />
           </div>
         </div>
 
@@ -40,9 +44,11 @@ export default function Home() {
           <p className="text-3xl">+</p>
         </div>
 
-        <div>
-          <div>
-            <EmojiPicker selectedEmoji={emoji2} emojiList={action_emojis} setEmoji={setEmoji2} />
+        <div className="flex flex-row w-1/4 gap-2">
+          <EmojiPicker selectedEmoji={emoji2} emojiList={action_emojis} setEmoji={setEmoji2} setEmojiEmotion={setEmoji2Emotion} setEmojiArousal={setEmoji2Arousal} />
+          <div className="flex flex-col w-2/3">
+            <EmotionSlider value={emoji2Emotion} defaultValue={[50]} onValueChange={setEmoji2Emotion} />
+            <ArouselSlider value={emoji2Arousal} defaultValue={[50]} onValueChange={setEmoji2Arousal} />
           </div>
         </div>
 
@@ -50,9 +56,11 @@ export default function Home() {
           <p className="text-3xl">+</p>
         </div>
 
-        <div>
-          <div>
-            <EmojiPicker selectedEmoji={emoji3} emojiList={action_emojis} setEmoji={setEmoji3} />
+        <div className="flex flex-row w-1/4 gap-2">
+          <EmojiPicker selectedEmoji={emoji3} emojiList={action_emojis} setEmoji={setEmoji3} setEmojiEmotion={setEmoji3Emotion} setEmojiArousal={setEmoji3Arousal} />
+          <div className="flex flex-col w-2/3">
+            <EmotionSlider value={emoji3Emotion} defaultValue={[50]} onValueChange={setEmoji3Emotion} />
+            <ArouselSlider value={emoji3Arousal} defaultValue={[50]} onValueChange={setEmoji3Arousal} />
           </div>
         </div>
 
@@ -65,7 +73,7 @@ export default function Home() {
         ) : (
           <div className="flex items-center" onClick={() => setLoading(true)}>
             <button className="bg-green-200 rounded-full text-white w-16 h-16 hover:bg-green-400 transition-colors duration-300">
-            <span className="text-black"> = </span>
+              <span className="text-black"> = </span>
             </button>
           </div>
         )}
@@ -73,7 +81,7 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col items-center">
-        <div> 
+        <div>
           {/* Open AI image */}
         </div>
 
--- src/components/EmojiPicker.tsx
+++ src/components/EmojiPicker.tsx
@@ -1,21 +1,20 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Button } from "@/components/ui/button"
 import {
     Dialog,
     DialogContent,
+    DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
 
-export default function EmojiPicker({ selectedEmoji, emojiList, setEmoji }: { selectedEmoji: string, emojiList: string[], setEmoji: (emoji: string) => void }) {
+export default function EmojiPicker({ selectedEmoji, emojiList, setEmoji, setEmojiEmotion, setEmojiArousal }: { selectedEmoji: string, emojiList: string[], setEmoji: (emoji: string) => void, setEmojiEmotion: (emotion: number[]) => void, setEmojiArousal: (arousal: number[]) => void }) {
     const [isDialogOpen, setIsDialogOpen] = useState(false);
-
-    useEffect(() => {
-        console.log(selectedEmoji)
-    }, [selectedEmoji])
 
     const pickEmoji = (emoji: string) => {
         if (!emojiList.includes(emoji)) return
         setEmoji(emoji)
+        setEmojiEmotion([50]);
+        setEmojiArousal([50]);
         setIsDialogOpen(false);
     }
 
@@ -25,6 +24,7 @@ export default function EmojiPicker({ selectedEmoji, emojiList, setEmoji }: { se
                 <Button className={"w-32 h-32 text-8xl bg-black/5 hover:bg-black/20"}>{selectedEmoji}</Button>
             </DialogTrigger>
             <DialogContent className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-6 p-4 h-[calc(75%)] w-full">
+                <DialogTitle className="sr-only">Select an emoji</DialogTitle>
                 {emojiList.map(emoji => (
                     <Button key={emoji} onClick={() => pickEmoji(emoji)} className={"w-16 h-16 text-4xl bg-black/0 hover:bg-black/20"}>
                         {emoji}
