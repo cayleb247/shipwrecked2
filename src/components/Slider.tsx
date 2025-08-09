f0b4fb7a0601890642c301d75bb356e4eaa034d0
Vedansh Mannem <mannem.vedansh@gmail.com>
2025-08-09 11:50:03 -0400

    update for merge

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -2,6 +2,8 @@ import styles from "./Sider.module.css"
 
 export default function Slider() {
     return (
-        <div style={styles.}></div>
+        <div>
+            
+        </div>
     )
 }
