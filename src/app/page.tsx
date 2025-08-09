6b3592f600f01b69a0f52ea7a43e31c700d81cf0
Nathan Cloutier <nathanc4571@gmail.com>
2025-08-09 09:09:10 -0400

    Unstylled emoji picker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,44 +2,27 @@
 
 import styles from "./page.module.css";
 import * as React from 'react';
+import EmojiPicker from '@/components/EmojiPicker';
 
 export default function Home() {
   const object_emojis = ["🚃", "👨", "🐕", "✈️", "🌮", "📕"];
   const action_emojis = ["🏃‍➡️", "🏊", "🚶‍♂️", "🤣"];
-  const [currentEmojiIndex, setCurrentEmojiIndex] = React.useState(0);
-  const [actionEmojiIndex, setActionEmojiIndex] = React.useState(0);
-
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentEmojiIndex((prev) => (prev + 1) % object_emojis.length);
-    }, 500);
-
-    return () => clearInterval(interval);
-  }, [object_emojis.length]);
-
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      setActionEmojiIndex((prev) => (prev + 1) % action_emojis.length);
-    }, 500);
-
-    return () => clearInterval(interval);
-  }, [action_emojis.length]);
 
   return (
     <div className={styles.page}>
       <div className={styles.emojiDiv}>
-        <div className={styles.emojiContainer} onClick={() => {}}>
+        <div className={styles.emojiContainer} onClick={() => { }}>
           <div className={styles.emojiBox}>
-            {object_emojis[currentEmojiIndex]}
+            <EmojiPicker emojis={object_emojis} />
           </div>
         </div>
-      <p>+</p>
+        <p>+</p>
         <div className={styles.emojiContainer}>
           <div className={styles.emojiBox}>
-            {action_emojis[actionEmojiIndex]}
+            <EmojiPicker emojis={action_emojis} />
           </div>
         </div>
-      <p>+</p>
+        <p>+</p>
         <div className={styles.emojiContainer}>
           <div className={styles.emojiBox}>
             🟩
@@ -47,7 +30,7 @@ export default function Home() {
         </div>
 
       </div>
-      
+
     </div>
   );
 }
